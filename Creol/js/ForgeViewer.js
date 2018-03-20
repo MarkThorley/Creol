4244abb643dce3fc361b20d3907ffb7b136e42b9
Mark Thorley <mark.thorley@designtech.io>
2018-03-20 15:28:32 +0000

    create an extension

--- Creol/js/ForgeViewer.js
+++ Creol/js/ForgeViewer.js
@@ -8,8 +8,11 @@ function launchViewer(urn) {
     var documentId = 'urn:' + urn;
     Autodesk.Viewing.Initializer(options, function onInitialized() {
         viewerApp = new Autodesk.Viewing.ViewingApplication('forgeViewer');
-        viewerApp.registerViewer(viewerApp.k3D, Autodesk.Viewing.Private.GuiViewer3D);
+        //viewerApp.registerViewer(viewerApp.k3D, Autodesk.Viewing.Private.GuiViewer3D);
+        var config3d = { extensions: ['MyAwesomeExtension'] };
+        viewerApp.registerViewer(viewerApp.k3D, Autodesk.Viewing.Private.GuiViewer3D, config3d);
         viewerApp.loadDocument(documentId, onDocumentLoadSuccess, onDocumentLoadFailure);
+
     });
 }
 
