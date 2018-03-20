b94544c6c92cb6733979514d505f74e567674968
Mark Thorley <mark.thorley@designtech.io>
2018-03-20 16:48:39 +0000

    add extension

--- Creol/js/ForgeViewer.js
+++ Creol/js/ForgeViewer.js
@@ -9,8 +9,27 @@ function launchViewer(urn) {
     Autodesk.Viewing.Initializer(options, function onInitialized() {
         viewerApp = new Autodesk.Viewing.ViewingApplication('forgeViewer');
         //viewerApp.registerViewer(viewerApp.k3D, Autodesk.Viewing.Private.GuiViewer3D);
-        var config3d = { extensions: ['MyAwesomeExtension'] };
-        viewerApp.registerViewer(viewerApp.k3D, Autodesk.Viewing.Private.GuiViewer3D, config3d);
+        //var config3d = { extensions: ['MyAwesomeExtension'] };
+        //var config3d = { extensions: ["Autodesk.Viewing.ZoomWindow"] };
+        var config = {
+            extensions: ["Autodesk.InViewerSearch"],
+            inViewerSearchConfig : {
+                uiEnabled: true,
+                clientId: "adsk.forge.default",
+                sessionId: "F969EB70-242F-11E6-BDF4-0800200C9A66",
+                loadedModelTab: {
+                    enabled: true,  // If false, the tab is hidden.
+                    displayName: 'This View',
+                    pageSize: 50
+                },
+                relatedItemsTab:{
+                    enabled: true,  // If false, the tab is hidden.
+                    displayName: 'This Item',
+                    pageSize: 20
+                }
+            }
+        };
+        viewerApp.registerViewer(viewerApp.k3D, Autodesk.Viewing.Private.GuiViewer3D, config);
         viewerApp.loadDocument(documentId, onDocumentLoadSuccess, onDocumentLoadFailure);
 
     });
